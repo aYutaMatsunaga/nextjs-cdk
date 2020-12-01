4897d7ac4fc73e445c58be8546b307eeb0e7d012
aYutaMatsunaga <yuta.matsunaga@access-company.com>
2020-12-01 11:57:41 +0900

    Remove next-app

---
+++ lambda/upload.ts
@@ -0,0 +1,44 @@
+import * as aws from 'aws-sdk'
+import * as fs from 'fs'
+import * as path from 'path'
+
+const config = {
+  s3BucketName: process.env.DEPLOY_BUCKET ?? '',
+  folderPath: './dist',
+}
+
+const s3 = new aws.S3()
+
+const distFolderPath = path.join(__dirname, config.folderPath)
+
+fs.readdir(distFolderPath, (err, files) => {
+  if (!files || files.length === 0) {
+    console.log(`provided folder '${distFolderPath}' is empty or does not exist.`)
+    console.log('Make sure your project was compiled!')
+    return
+  }
+
+  for (const fileName of files) {
+    const filePath = path.join(distFolderPath, fileName)
+
+    // ignore if directory
+    if (fs.lstatSync(filePath).isDirectory()) {
+      continue
+    }
+
+    // read file contents
+    fs.readFile(filePath, (error, fileContent) => {
+      // if unable to read file contents, throw exception
+      if (error) { throw error }
+
+      // upload file to S3
+      s3.putObject({
+        Bucket: config.s3BucketName,
+        Key: fileName,
+        Body: fileContent,
+      }, (_res) => {
+        console.log(`Successfully uploaded '${fileName}'!`)
+      })
+    })
+  }
+})
--- lib/nextjs-cdk-stack.ts
+++ lib/nextjs-cdk-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as s3 from '@aws-cdk/aws-s3'
 
 export class NextjsCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'NextjsAppBucket')
+
+    const handler = new lambda.Function(this, 'BuildNextjs', {
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'index.handler',
+      runtime: lambda.Runtime.NODEJS_12_X,
+      memorySize: 256,
+      timeout: cdk.Duration.minutes(5),
+      environment: {
+        DEPLOY_BUCKET: bucket.bucketName,
+        SOURCE_URL: '',
+      },
+    })
+
+    bucket.grantWrite(handler)
   }
 }
